c21822b0c5c8d165d62be2141ad4d8a6c2128fe6
DeadHeadCore <deadheadcore@gmail.com>
2019-04-08 02:16:27 +0500

    sql backup

---
+++ sql_backup/shorturl.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 08 2019 г., 00:11
+-- Версия сервера: 5.7.11
+-- Версия PHP: 7.0.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `shorturl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(11) NOT NULL,
+  `url_id` int(11) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `c_counter` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `short_urls`
+--
+
+CREATE TABLE IF NOT EXISTS `short_urls` (
+  `id` int(10) unsigned NOT NULL,
+  `long_url` varchar(255) NOT NULL,
+  `short_code` varbinary(6) NOT NULL,
+  `date_created` int(10) unsigned NOT NULL,
+  `url_live` int(10) unsigned NOT NULL,
+  `counter` int(10) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_agents`
+--
+
+CREATE TABLE IF NOT EXISTS `users_agents` (
+  `id` int(11) NOT NULL,
+  `url_id` int(11) NOT NULL,
+  `agent` varchar(255) NOT NULL,
+  `a_counter` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `url_id` (`url_id`);
+
+--
+-- Индексы таблицы `short_urls`
+--
+ALTER TABLE `short_urls`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `short_code` (`short_code`);
+
+--
+-- Индексы таблицы `users_agents`
+--
+ALTER TABLE `users_agents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `url_id` (`url_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `short_urls`
+--
+ALTER TABLE `short_urls`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `users_agents`
+--
+ALTER TABLE `users_agents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
